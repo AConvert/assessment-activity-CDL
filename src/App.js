db34fd99be42b78100e5b91993b2ac8240ca2c5f
AConvert <68063405+AConvert@users.noreply.github.com>
2023-03-29 12:21:48 +0100

    integrate taiwind css to speed up the UI stilyng

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       name: A,
       id: 1,
       price: 50,
+      quantity: 1,
       image:
         "https://images.unsplash.com/photo-1560806887-1e4cd0b6cbd6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXBwbGVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     },
@@ -16,6 +17,7 @@ function App() {
       name: B,
       id: 2,
       price: 30,
+      quantity: 1,
       image:
         "https://images.unsplash.com/photo-1514756331096-242fdeb70d4a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVhcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
     },
@@ -23,6 +25,7 @@ function App() {
       name: C,
       id: 3,
       price: 20,
+      quantity: 1,
       image:
         "https://images.unsplash.com/photo-1572539280469-9c738c59964d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8a2l3aXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
     },
@@ -30,15 +33,25 @@ function App() {
       name: D,
       id: 4,
       price: 15,
+      quantity: 1,
       image:
         "https://images.unsplash.com/photo-1590502593747-42a996133562?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bGVtb25zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     },
   ];
 
   return (
-    <main className="app">
-      <h1>Checkout System</h1>
-      <Product />
+    <main>
+      <h1 className="text-center">Checkout System</h1>
+      {items.map(({ name, price, quantity, id }) => (
+        <Product
+          name={name}
+          key={id}
+          id={id}
+          price={price}
+          image={image}
+          quantity={quantity}
+        />
+      ))}
       <Basket />
     </main>
   );
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-function Product() {
+function Product({ name, price, image, id, quantity }) {
   return (
     <main>
-      <section>
-        <img src="" />
-        <h3>Price: 50p</h3>
+      <section className="">
+        <div>
+          <img src={image} />
+          <div>
+            <h1>{name}</h1>
+            <h3>Price: {price}</h3>
+            <h2>{quantity}</h2>
+          </div>
+        </div>
+
         <button>Add to basket</button>
       </section>
     </main>
