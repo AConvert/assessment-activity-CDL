91620d8ef077857c8a0c81977a5ecb261cb0bf15
AConvert <68063405+AConvert@users.noreply.github.com>
2023-03-29 15:14:24 +0100

    styled product element and imported props inot it

--- src/App.js
+++ src/App.js
@@ -31,18 +31,23 @@ function App() {
   ];
 
   return (
-    <main>
-      <h1 className="text-center">Checkout System</h1>
-      {items.map(({ name, price, quantity, id }) => (
-        <Product
-          name={name}
-          key={id}
-          id={id}
-          price={price}
-          quantity={quantity}
-        />
-      ))}
-      <Basket />
+    <main className="w-screen h-screen ">
+      <h1 className="text-center text-4xl font-bold py-5">Checkout System</h1>
+      <section className="flex justify-center items-center">
+        <div className="flex flex-col ">
+          <h1 className="text-3xl text-black font-semibold py-3">Items</h1>
+          {items.map(({ name, price, quantity, id }) => (
+            <Product
+              name={name}
+              key={id}
+              id={id}
+              price={price}
+              quantity={quantity}
+            />
+          ))}
+        </div>
+        <Basket />
+      </section>
     </main>
   );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,17 +2,32 @@ import React from "react";
 
 function Product({ name, price, id, quantity }) {
   return (
-    <main id={id}>
-      <section className="flex items-center text-black">
+    <main
+      id={id}
+      className="shadow-sm shadow-gray-500 rounded-lg w-96 py-3 px-3 my-3 hover:scale-105"
+    >
+      <section className="flex items-end space-x-36 text-black">
         <div>
-          <div>
-            <h1>{name}</h1>
-            <h3>Price: {price}</h3>
-            <h2>{quantity}</h2>
+          <div className="flex flex-col space-y-1">
+            <h1 className="font-semibold text-xl">{name}</h1>
+            <h3 className="text-md font-semibold">
+              Price: <span className="font-semibold">{price} pp</span>
+            </h3>
+            <div className="flex items-center space-x-2">
+              <button className="px-2 rounded-full shadow-sm shadow-gray-500 focus:bg-gray-500 focus:text-white">
+                -
+              </button>
+              <h2>{quantity}</h2>
+              <button className="px-2 rounded-full shadow-sm shadow-gray-500 focus:bg-gray-500 focus:text-white">
+                +
+              </button>
+            </div>
           </div>
         </div>
 
-        <button>Add to basket</button>
+        <button className="bg-yellow-400 px-3 py-1 text-md rounded-lg">
+          Add to basket
+        </button>
       </section>
     </main>
   );
