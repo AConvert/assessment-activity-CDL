5c6642f9998d026ac547a8090cf5edef64da77d5
AConvert <68063405+AConvert@users.noreply.github.com>
2023-03-29 12:13:13 +0100

    create an array of items

--- src/App.js
+++ src/App.js
@@ -9,21 +9,29 @@ function App() {
       name: A,
       id: 1,
       price: 50,
+      image:
+        "https://images.unsplash.com/photo-1560806887-1e4cd0b6cbd6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXBwbGVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     },
     {
       name: B,
       id: 2,
       price: 30,
+      image:
+        "https://images.unsplash.com/photo-1514756331096-242fdeb70d4a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVhcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
     },
     {
       name: C,
       id: 3,
       price: 20,
+      image:
+        "https://images.unsplash.com/photo-1572539280469-9c738c59964d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8a2l3aXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
     },
     {
       name: D,
       id: 4,
       price: 15,
+      image:
+        "https://images.unsplash.com/photo-1590502593747-42a996133562?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bGVtb25zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     },
   ];
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Product() {
-  return <div>Product</div>;
+  return (
+    <main>
+      <section>
+        <img src="" />
+        <h3>Price: 50p</h3>
+        <button>Add to basket</button>
+      </section>
+    </main>
+  );
 }
 
 export default Product;
